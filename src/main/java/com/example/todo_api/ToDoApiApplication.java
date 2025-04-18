9182511c1e8a6a86b98fa31e55d08cfcc81616af
Eduards <Eduardik1915@gmail.com>
2025-04-18 14:41:11 +0300

    Little fixes

--- src/main/java/com/example/todo_api/ToDoApiApplication.java
+++ src/main/java/com/example/todo_api/ToDoApiApplication.java
@@ -1,6 +1,5 @@
 package com.example.todo_api;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ToDoApiApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
-
 		SpringApplication.run(ToDoApiApplication.class, args);
 	}
 
