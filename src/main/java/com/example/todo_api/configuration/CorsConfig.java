e918ed19a1921d2c30b01d241a45bb1f90f3ee81
Eduards <Eduardik1915@gmail.com>
2025-04-21 19:31:45 +0300

    fix

--- src/main/java/com/example/todo_api/configuration/CorsConfig.java
+++ src/main/java/com/example/todo_api/configuration/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**");
 //                        .allowedOrigins("*")
 //                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
