de6b921bddcf569340f5ea932ee0a22432a53992
Eduards <Eduardik1915@gmail.com>
2025-04-21 20:27:02 +0300

    fix

--- src/main/java/com/example/todo_api/configuration/CorsConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.todo_api.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer{
-
-}
