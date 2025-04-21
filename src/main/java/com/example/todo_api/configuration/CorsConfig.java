b5cce219f61b992097c3163d21020a5889b12714
Eduards <Eduardik1915@gmail.com>
2025-04-21 19:23:54 +0300

    fix

--- src/main/java/com/example/todo_api/configuration/CorsConfig.java
+++ src/main/java/com/example/todo_api/configuration/CorsConfig.java
@@ -2,6 +2,7 @@ package com.example.todo_api.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,11 +12,11 @@ public class CorsConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**");
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*");
             }
         };
     }
