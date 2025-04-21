6546abb28bcb3c2c8af16f95e86bac21c07a6aff
Eduards <Eduardik1915@gmail.com>
2025-04-21 20:24:20 +0300

    fix

--- src/main/java/com/example/todo_api/configuration/CorsConfig.java
+++ src/main/java/com/example/todo_api/configuration/CorsConfig.java
@@ -1,17 +1,9 @@
-//package com.example.todo_api.configuration;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer{
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**") // Разрешаем CORS для всех URL
-//                .allowedOriginPatterns("*") // Используем allowedOriginPatterns вместо allowedOrigins для гибкости
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Все стандартные методы
-//                .allowedHeaders("*") // Разрешаем любые заголовки
-//                .allowCredentials(false); // Без куки и авторизации — безопаснее и проще
-//    }
-//}
+package com.example.todo_api.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer{
+
+}
